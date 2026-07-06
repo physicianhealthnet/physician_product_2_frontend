1fc1d7074a3ba029aaec4ca7ba7d5ca09beba0f6
navadeepak <navadeepakaswin007@gmail.com>
2026-07-06 18:27:38 +0530

    axios

--- src/utilities/AxiosInstance.js
+++ src/utilities/AxiosInstance.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  // baseURL: "https://demo2.physicianhealthnet.com/api"
-  baseURL: "http://localhost:4026"
+  baseURL: "https://demo2.physicianhealthnet.com/api"
+  // baseURL: "http://localhost:4026"
 });
 
 const AxiosInstanceSecondryServer = axios.create({
-  // baseURL: "https://dependencyforphn.physicianhealthnet.com/api/"
-  baseURL: "http://localhost:3028",
+  baseURL: "https://dependencyforphn.physicianhealthnet.com/api/"
+  // baseURL: "http://localhost:3028",
 });
 
 const AxiosInstanceDependency = AxiosInstanceSecondryServer;
